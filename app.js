7c216ad47d15efdbe8e6b801b3e39b7c1d593291
Reed Whetstone <rwhetstone0934@gmail.com>
2022-03-17 12:31:22 -0600

    added bootstrap and basic template

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -16,6 +17,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
