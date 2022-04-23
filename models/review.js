7c5a23a1787a467cecad038d4636051f6088474f
Reed Whetstone <rwhetstone0934@gmail.com>
2022-04-23 16:01:57 -0600

    Added author permissions to reviews

--- models/review.js
+++ models/review.js
@@ -1,11 +1,15 @@
 /** @format */
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
   body: String,
   rating: Number,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
-module.exports = mongoose.model("Review", reviewSchema);
+module.exports = mongoose.model('Review', reviewSchema);
