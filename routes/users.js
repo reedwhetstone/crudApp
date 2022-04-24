3c3aa29d172c45cc7780137ceb75ea68ddcab873
Reed Whetstone <rwhetstone0934@gmail.com>
2022-04-23 21:59:08 -0600

    Users controller
    added a users controller

--- routes/users.js
+++ routes/users.js
@@ -3,50 +3,17 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const User = require('../models/user');
 const catchAsync = require('../utilities/catchAsync');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post(
-  '/register',
-  catchAsync(async (req, res) => {
-    try {
-      const { username, password, email } = req.body;
-      const user = new User({
-        email,
-        username,
-      });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash('success', `Welcome, ${registeredUser.username}`);
-        res.redirect('/campgrounds');
-      });
-    } catch (e) {
-      req.flash('error', e.message);
-      res.redirect('register');
-    }
-  })
-);
+router.post('/register', catchAsync(users.registerUser));
 
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  req.flash('success', `Welcome back, ${req.user.username}`);
-  const redirectUrl = req.session.returnTo || '/campgrounds';
-  delete req.session.returnTo;
-  res.redirect(redirectUrl);
-});
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser);
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success', 'Logout Successful.');
-  res.redirect('/campgrounds');
-});
+router.get('/logout', users.logoutUser);
 
 module.exports = router;
