0bb366d8eb77ccfdd9ec403238be424a1feb618c
Reed Whetstone <rwhetstone0934@gmail.com>
2022-04-23 23:03:02 -0600

    Made the index page a bit prettier
    index page needed some sprucing up. tried to impliment a hover animation
    but couldn't get it right, will have to return to it later. limited the
    description to 175 characters to create a more unified look. other small
    changes.

---
+++ public/javascripts/mouseoverIndex.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('mouseover', () => {
+    removeActive();
+    panel.classList.add('active');
+  });
+});
+
+function removeActive() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+    console.log('event');
+  });
+}
