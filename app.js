bffe9d7542767967191d715bc79e624fb5859442
Reed Whetstone <rwhetstone0934@gmail.com>
2022-05-03 12:15:43 -0600

    Fixed gitignore stuff, getting ready to deploy

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ const reviewRoutes = require('./routes/reviews');
 
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const MongoStore = require('connect-mongo');
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
@@ -45,9 +48,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }));
 
+const secret = process.env.SECRET || 'secret';
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: secret,
+  },
+});
+
+store.on('error', function (e) {
+  console.log('Session Store Error!', e);
+});
+
 const sessionConfig = {
+  store: store,
   name: 'session',
-  secret: 'secret',
+  secret: secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
